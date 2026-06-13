459dfcf0f8c94eba0c66815ab21ddffff5abf24d
CHamod-MAdusanka <madusankac965@gmail.com>
2026-06-13 11:36:14 +0530

    Update exsample01.py

--- exsample01.py
+++ exsample01.py
@@ -1,2 +1,2 @@
 import FreeSimpleGUI as sg
-sg.popup_animated("Hello World", title="MY first animation", animation="loading.gif", time=5000)
+sg.popup_animated("HELLO WORLD", title="HELLO WORLD", time=2000)
